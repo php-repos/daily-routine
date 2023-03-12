5b23ca7cfacc90134de969a6c7d47c0718fe9d5a
Morteza Poussaneh <49806042+MortezaPoussaneh@users.noreply.github.com>
2023-03-12 18:21:52 +0100

    debug mac

--- Public/index.php
+++ Public/index.php
@@ -7,14 +7,18 @@ use PhpRepos\DailyRoutines\Kernel\IO\Response\Status;
 use PhpRepos\DailyRoutines\Kernel\Router;
 use PhpRepos\DailyRoutines\Kernel\UI\Html;
 
-$application = Bootstrap\boot();
-$request = Utils\parse_http();
+try {
+    $application = Bootstrap\boot();
+    $request = Utils\parse_http();
 
-$endpoint = Router\http($request, $application->routes);
-$response = is_callable($endpoint)
-    ? call_user_func($endpoint, $request)
-    : Response::html(Html\view('404'), status: Status::NOT_FOUND);
+    $endpoint = Router\http($request, $application->routes);
+    $response = is_callable($endpoint)
+        ? call_user_func($endpoint, $request)
+        : Response::html(Html\view('404'), status: Status::NOT_FOUND);
 
-http_response_code($response->header->status->value);
-header('Content-Type: ' . $response->header->content_type);
-echo $response->body->content;
+    http_response_code($response->header->status->value);
+    header('Content-Type: ' . $response->header->content_type);
+    echo $response->body->content;
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+}
