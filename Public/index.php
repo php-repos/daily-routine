1b691a00e077eefb1c7c48f7afbd77543be0ae6c
Morteza Poussaneh <49806042+MortezaPoussaneh@users.noreply.github.com>
2023-03-14 08:09:19 +0100

    Handle exception

--- Public/index.php
+++ Public/index.php
@@ -7,23 +7,19 @@ use PhpRepos\DailyRoutines\Kernel\IO\Response\Status;
 use PhpRepos\DailyRoutines\Kernel\Router;
 use PhpRepos\DailyRoutines\Kernel\UI\Html;
 
+try {
+    $application = Bootstrap\boot();
+    $request = Utils\parse_http();
 
-error_log('request received', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-error_log('Booting application', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-$application = Bootstrap\boot();
-error_log('create request', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-$request = Utils\parse_http();
+    $endpoint = Router\http($request, $application->routes);
+    $response = is_callable($endpoint)
+        ? call_user_func($endpoint, $request)
+        : Response::html(Html\view('404'), status: Status::NOT_FOUND);
 
-error_log('find route', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-$endpoint = Router\http($request, $application->routes);
-error_log('check endpoint', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-$response = is_callable($endpoint)
-    ? call_user_func($endpoint, $request)
-    : Response::html(Html\view('404'), status: Status::NOT_FOUND);
+    http_response_code($response->header->status->value);
+    header('Content-Type: ' . $response->header->content_type);
+    echo $response->body->content;
+} catch (Exception $exception) {
+    file_put_contents(\PhpRepos\FileManager\Resolver\root() . '/logs.txt', $exception->getMessage());
+}
 
-error_log('send response header', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-http_response_code($response->header->status->value);
-header('Content-Type: ' . $response->header->content_type);
-error_log('send response body', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
-echo $response->body->content;
-error_log('response sent', 3, \PhpRepos\FileManager\Resolver\root() . '/logs.txt');
